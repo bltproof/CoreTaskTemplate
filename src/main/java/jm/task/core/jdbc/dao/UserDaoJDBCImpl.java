f14ffa5ace891359c4d8a5b0977e2cb5ee082e7e
bltproof <aka.vincente.vega@gmail.com>
2020-12-04 22:11:26 +0300

    task is done

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,109 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private Connection connection = Util.getInstance().getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
+    @Override
     public void createUsersTable() {
+        String query = "CREATE TABLE user (id INT NOT NULL AUTO_INCREMENT, name VARCHAR (100) NOT NULL,\n" +
+                "lastName VARCHAR (100) NOT NULL,\n" +
+                "age INT NOT NULL,\n" +
+                "PRIMARY KEY (id))";
 
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void dropUsersTable() {
+        String query = "DROP TABLE IF EXISTS user";
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
+        String query = "INSERT INTO USER (NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+
+            System.out.println("User с именем - " + name + " добавлен в базу данных.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void removeUserById(long id) {
+        String query = "DELETE FROM user WHERE id = '" + id + "'";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        String query = "SELECT * FROM user";
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                User user = new User();
+
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+
+                userList.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return userList;
     }
 
     public void cleanUsersTable() {
+        String query = "DELETE FROM user";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static Util instance;
+    private Connection connection;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysqldb?useUnicode=true&serverTimezone=UTC";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root1234";
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private Util() {
+        try {
+            Class.forName(DRIVER);
+            this.connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            System.out.println("Connection established");
+
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Database Connection Creation Failed : " + e.getMessage());
+        }
+    }
+
+    public static Util getInstance() {
+        try {
+            if (instance == null) {
+                instance = new Util();
+            } else if (instance.getConnection().isClosed()) {
+                instance = new Util();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return instance;
+    }
+
 }
