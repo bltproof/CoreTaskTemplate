1ebead07938b80a4f931be0b76ce71b44097c114
bltproof <aka.vincente.vega@gmail.com>
2020-12-06 15:09:53 +0300

    правки

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,23 +2,16 @@ package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.HashMap;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static Connection connection;
-    private static SessionFactory sessionFactory;
-    private static StandardServiceRegistry registry;
-
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/mysqldb?useUnicode=true&serverTimezone=UTC";
     private static final String LOGIN = "root";
@@ -26,40 +19,23 @@ public class Util {
 
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        properties.put(Environment.HBM2DDL_AUTO, "create");
+        properties.put(Environment.DRIVER, DRIVER);
+        properties.put(Environment.USER, LOGIN);
+        properties.put(Environment.PASS, PASSWORD);
+        properties.put(Environment.URL, URL);
 
-                HashMap<String, String> dbSettings = new HashMap<>();
-                dbSettings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                dbSettings.put(Environment.HBM2DDL_AUTO, "update");
-                dbSettings.put(Environment.DRIVER, DRIVER);
-                dbSettings.put(Environment.USER, LOGIN);
-                dbSettings.put(Environment.PASS, PASSWORD);
-                dbSettings.put(Environment.URL, URL);
+        Configuration cfg = new Configuration();
+        cfg.setProperties(properties);
+        cfg.addAnnotatedClass(User.class);
 
-                registryBuilder.applySettings(dbSettings);
-
-                registry = registryBuilder.build();
-
-                MetadataSources sources = new MetadataSources(registry).addAnnotatedClass(User.class);
-                Metadata metadata = sources.getMetadataBuilder().build();
-
-                sessionFactory = metadata.getSessionFactoryBuilder().build(); //удаляет предыдущие записи из таблицы??
-
-            } catch (Exception e) {
-                e.printStackTrace();
-
-                if (registry != null) {
-                    StandardServiceRegistryBuilder.destroy(registry);
-                }
-
-            }
-        }
-        return sessionFactory;
+        return cfg.buildSessionFactory();
     }
 
     public static Connection getJdbcConnection() {
+        Connection connection = null;
         try {
             Class.forName(DRIVER);
             connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
