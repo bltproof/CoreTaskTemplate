05465c6f4ad4547db9318b3cbed0d6843798d247
bltproof <aka.vincente.vega@gmail.com>
2020-12-06 15:30:59 +0300

    правки

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,14 +6,13 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static Util instance;
-    private Connection connection;
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/mysqldb?useUnicode=true&serverTimezone=UTC";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "root1234";
 
-    public Connection getJdbcConnection() {
+    public static Connection getJdbcConnection() {
+        Connection connection = null;
         try {
             Class.forName(DRIVER);
             connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
@@ -24,18 +23,4 @@ public class Util {
         }
         return connection;
     }
-
-    public static Util getInstance() {
-        try {
-            if (instance == null) {
-                instance = new Util();
-            } else if (instance.getJdbcConnection().isClosed()) {
-                instance = new Util();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return instance;
-    }
-
 }
